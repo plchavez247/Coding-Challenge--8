643017cdab6473669344982d5b17b216e9379dcc
Pablo Chavez <plchavez@usf.edu>
2025-02-16 11:05:15 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -7,3 +7,13 @@
  calculateSalary(5000, 500, 0.1);
  calculateSalary(7000, 1000, 0.15); //Using given test data to see expected output
 
+ //Task 2: Function Expression
+
+ const calculateDiscount = function (price, discountRate) {
+    let finalPrice = price - (price * discountRate);// Using formula to compute final price
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`);// logging final price with toFixed to round to two decimal places
+    
+ };
+
+ calculateDiscount(100, 0.2);
+ calculateDiscount(250, 0.15);//Using given test data
